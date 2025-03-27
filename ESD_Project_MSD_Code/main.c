8e206b9339ad0618c9b0f95c960c3ef79364b8e4
Charlotte Studio <charlottexprojects@gmail.com>
2025-03-28 02:26:58 +0800

    add normal mode function

--- ESD_Project_MSD_Code/main.c
+++ ESD_Project_MSD_Code/main.c
@@ -2,34 +2,41 @@
 #include <stdbool.h>
 
 extern void ClearDisplay();
-extern void DisplayChar(int, int);
+extern void DisplayChar();
+extern void DisplayNumber();
 extern void StartDisplay();
+
+#define Button_StartStop  BIT1  // P1.1 Button
+#define Button_LapReset   BIT5  // P4.5 Button
+#define Button_Mode       BIT3  // P1.3 Button
+
+volatile bool onClickButtonMode = false;
+
+#define NormalMode       0
+#define TimeSettingMode  1
+#define AlarmSettingMode 2
+#define ChronoMode       3
+//#define AlarmMode        
+
+volatile unsigned int currentMode = 0;
+
 
 // 使用 volatile，防止编译器优化
 volatile bool start = false;
 
+volatile unsigned int chrono_milliseconds = 0;
+volatile unsigned int chrono_seconds      = 0;
+volatile unsigned int chrono_minutes      = 0;
+
 volatile unsigned int milliseconds = 0;
-volatile unsigned int seconds = 0;
-volatile unsigned int minutes = 0;
-
-#define BUTTON1 BIT1  // P1.1 Button
-#define BUTTON2 BIT5  // P4.5 Button
-
+volatile unsigned int seconds      = 0;
+volatile unsigned int minutes      = 0;
+volatile unsigned int hours        = 0;
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
-    PM5CTL0 &= ~LOCKLPM5;
-
-    // set up Button P1.1
-    P1DIR &= ~BUTTON1;              // set   P1.1 to input
-    P1REN |= BUTTON1;               // using P1.1 up
-    P1OUT |= BUTTON1;               // set   P1.1 up
-
-    // set up Button P4.5
-    P4DIR &= ~BUTTON2;              // set   P4.5 to input
-    P4REN |= BUTTON2;               // using P4.5 up
-    P4OUT |= BUTTON2;               // set   P4.5 up
+    Init();
+    SetUpButton();
 
 ////////////////////////////////////////////////////////////////////////////////////
 
@@ -63,35 +70,108 @@ int main(void)
 
     _BIS_SR(GIE);
 
+
     while (1) {
 
-        if ((P1IN & BUTTON1) == 0)
+        if ((P1IN & Button_StartStop) == 0)
         {
             start = true;
+            //printf("Button_StartStop");
         }
 
-        if ((P4IN & BUTTON2) == 0)
+        if ((P4IN & Button_LapReset) == 0)
         {
             start = false;
+            //printf("Button_StartStop");
         }
 
-        volatile unsigned int i;
-        for (i = 0; i < 10; i++) {}
+        CheckOnClickButtonMode();
 
-        UpdateStopwatchTime();
-        ShowStopwatch();
+        volatile unsigned int i;
+        for (i = 0; i < 100; i++) {}
+
+        switch (currentMode) {
+            case NormalMode:
+                UpdateNormalTimer();
+                ShowNormalTimer();
+                break;
+            case ChronoMode:
+                UpdateStopwatchTimer();
+                ShowStopwatch();
+                break;
+        }
     }
 }
 
 // Timer_A0 is a Stopwatch Timer
 #pragma vector=TIMER0_A0_VECTOR
-__interrupt void Timer_A0_ISR(void) {
+__interrupt void Timer_A0_ISR(void) 
+{
+    milliseconds++;
     if (start)
-        milliseconds++;
+        chrono_milliseconds++;
 }
 
 
-void UpdateStopwatchTime(){
+void Init(){
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    PM5CTL0 &= ~LOCKLPM5;
+
+    currentMode = NormalMode;
+    onClickButtonMode = false;
+
+    ResetChrono();
+    
+    minutes = 14;
+    hours = 2;
+}
+
+void ResetChrono(){
+    chrono_milliseconds = 0;
+    chrono_seconds      = 0;
+    chrono_minutes      = 0;
+}
+
+void SetUpButton(){
+    // set up Button P1.1
+    P1DIR &= ~Button_StartStop;     // set P1.1 to input
+    P1REN |= Button_StartStop;      // use P1.1 up
+    P1OUT |= Button_StartStop;      // set P1.1 up
+
+    // set up Button P4.5
+    P4DIR &= ~Button_LapReset;       // set P4.5 to input
+    P4REN |= Button_LapReset;        // usw P4.5 up
+    P4OUT |= Button_LapReset;        // set P4.5 up
+
+    // set up Button P1.3
+    P1DIR &= ~Button_Mode;           // set P1.3 to input
+    P1REN |= Button_Mode;            // use P1.3 up
+    P1OUT |= Button_Mode;            // set P1.3 up
+}
+
+void CheckOnClickButtonMode(){
+    if ((P1IN & Button_Mode) == 0)
+    {
+        if (!onClickButtonMode)
+            onClickButtonMode = true;
+    } 
+    else 
+    {
+        if (onClickButtonMode)
+        {
+            onClickButtonMode = false;
+            currentMode++;
+            
+            if (currentMode == ChronoMode)
+                ResetChrono();
+
+            if (currentMode > ChronoMode)
+                currentMode = NormalMode;
+        }
+    }
+}
+
+void UpdateNormalTimer(){
     if(milliseconds >= 1000)
     {
         milliseconds = 0;
@@ -105,18 +185,62 @@ void UpdateStopwatchTime(){
             if(minutes >= 60)
             {
                 minutes = 0;
+                hours++;
+
+                if (hours >= 24)
+                {
+                    hours = 0;
+                }
             }
         }
     }
 }
 
+void UpdateStopwatchTimer(){
+    if(chrono_milliseconds >= 1000)
+    {
+        chrono_milliseconds = 0;
+        chrono_seconds++;
+        
+        if(chrono_seconds >= 60)
+        {
+            chrono_seconds = 0;
+            chrono_minutes++;
+            
+            if(chrono_minutes >= 60)
+                chrono_minutes = 0;
+        }
+    }
+}
+
+void ShowNormalTimer(){
+    ClearDisplay();
+
+    DisplayNumber((hours / 10), 10, 1);
+    DisplayNumber((hours % 10), 10, 3);
+
+    if ((seconds % 2) == 0)
+        DisplayChar(20, 10, 5);
+    else
+        DisplayChar(2, 10, 5);
+
+    DisplayNumber((minutes / 10), 10, 6);
+    DisplayNumber((minutes % 10), 10, 8);
+
+    //DisplayNumber((sec / 10), 10, 11);
+    //DisplayNumber((sec % 10), 10, 13);
+    //DisplayNumber((milliseconds / 100), 10, 11);
+    //DisplayNumber(((milliseconds / 10) % 10), 10, 13);
+    StartDisplay();
+}
+
 void ShowStopwatch(){
     ClearDisplay();
-    DisplayNumber((minutes / 10), 10, 1);
-    DisplayNumber((minutes % 10), 10, 3);
-    DisplayNumber((seconds / 10), 10, 6);
-    DisplayNumber((seconds % 10), 10, 8);
-    DisplayNumber((milliseconds / 100), 10, 11);
-    DisplayNumber(((milliseconds / 10) % 10), 10, 13);
+    DisplayNumber((chrono_minutes / 10), 10, 1);
+    DisplayNumber((chrono_minutes % 10), 10, 3);
+    DisplayNumber((chrono_seconds / 10), 10, 6);
+    DisplayNumber((chrono_seconds % 10), 10, 8);
+    DisplayNumber((chrono_milliseconds / 100), 10, 11);
+    DisplayNumber(((chrono_milliseconds / 10) % 10), 10, 13);
     StartDisplay();
 }
