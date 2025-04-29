6eb79dd2b60e9a2b70b3872c70185b342f697a05
Charlotte Studio <charlottexprojects@gmail.com>
2025-04-29 15:57:56 +0800

    update app

--- ESD_Project_Unity/Assets/Scripts/UIManager.cs
+++ ESD_Project_Unity/Assets/Scripts/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -7,6 +8,8 @@ using FirebaseTools;
 public class UIManager : MonoBehaviour
 {
     private const string AlarmSettingPath = "/MSP430_Project/Alarm_Setting";
+    private const string TimeZonePath = "/MSP430_Project/TimeZone";
+
     private const string PWKey = "p_ey";
     private const string AccountKey = "a_ey";
     
@@ -21,11 +24,22 @@ public class UIManager : MonoBehaviour
     [SerializeField] private SimpleButton _simpleButton;
     
     [Header("Main Page")]
+    [SerializeField] private TMP_Text _timerText;
+    
     [SerializeField] private TMP_Text _alarmText;
     [SerializeField] private TMP_InputField _alarmInputField;
     [SerializeField] private SimpleButton _alarmButton;
+    
+    [SerializeField] private TMP_Text _timeZoneText;
+    [SerializeField] private TMP_InputField _timeZoneInputField;
+    [SerializeField] private SimpleButton _timeZoneButton;
+    
     [SerializeField] private SimpleButton _renewButton;
 
+    [SerializeField] private int _timeZone = 8;
+    private bool showColon = true;
+    private float updateInterval = 1f;
+    private float timer = 0f;
     
     private void Start()
     {
@@ -42,10 +56,37 @@ public class UIManager : MonoBehaviour
             _ = SaveAlarmSetting();
         });
         
+        _timeZoneButton.AddOnClickAction(()=>
+        {
+            _ = SaveTimeZoneSetting();
+        });
+        
         _renewButton.AddOnClickAction(()=>
         {
             _ = InitMainPage();
         });
+    }
+    
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        
+        if (timer >= updateInterval)
+        {
+            timer = 0f;
+            UpdateTimeDisplay();
+        }
+    }
+    
+    private void UpdateTimeDisplay()
+    {
+        DateTime utcNow = DateTime.UtcNow;
+        DateTime utc8Time = utcNow.AddHours(_timeZone);
+        
+        showColon = !showColon;
+        string timeString = utc8Time.ToString("HH") + (showColon ? ":" : " ") + utc8Time.ToString("mm");
+        
+        _timerText.text = timeString;
     }
 
     private async Task OnClickLogin()
@@ -67,7 +108,10 @@ public class UIManager : MonoBehaviour
             PlayerPrefs.SetString(PWKey, userAccount);
             PlayerPrefs.SetString(AccountKey,pw);
             
+            Debug.Log("1");
+
             await InitMainPage();
+            Debug.Log("10");
 
             _popupDialog.StartPopupDialog("Login successful", onYesAction: () =>
             {
@@ -86,6 +130,8 @@ public class UIManager : MonoBehaviour
 
     private async Task InitMainPage()
     {
+        Debug.Log("2");
+
         var result = await RealtimeDatabaseManager.Instance.GetValue(AlarmSettingPath);
         if (result != null)
         {
@@ -98,6 +144,21 @@ public class UIManager : MonoBehaviour
             Debug.LogError("Get Alarm Setting Error");
             _alarmText.text = "Alarm Setting : -";
         }
+        Debug.Log("3");
+
+        var resultTimeZone = await RealtimeDatabaseManager.Instance.GetValue(TimeZonePath);
+        if (resultTimeZone != null)
+        {
+            var str = resultTimeZone.Value.ToString();
+            _timeZoneText.text = "Current Time Zone : " + str;
+        }
+        else
+        {
+            Debug.LogError("Get Time Zone Error");
+            _timeZoneText.text = "Current Time Zone : -";
+        }
+        Debug.Log("4");
+
     }
 
     private async Task SaveAlarmSetting()
@@ -115,5 +176,26 @@ public class UIManager : MonoBehaviour
         var str = alarmText;
         str = str.Replace(',', ':');
         _alarmText.text = "Alarm Setting : " + str;
+    }
+    
+    private async Task SaveTimeZoneSetting()
+    {
+        var timeZoneText = _timeZoneInputField.text;
+        if (timeZoneText == "")
+        {
+            _popupDialog.StartPopupDialog("Wrong time zone input. Please try again.");
+            return;
+        }
+            
+        await RealtimeDatabaseManager.Instance.SetValue(TimeZonePath, timeZoneText);
+        _popupDialog.StartPopupDialog("Save Time zone Done");
+
+        var str = timeZoneText;
+        _timeZoneText.text = "Current Time Zone : " + str;
+
+        if (int.TryParse(timeZoneText, out int t))
+            _timeZone = t;
+        else
+            Debug.Log(timeZoneText);
     }
 }
